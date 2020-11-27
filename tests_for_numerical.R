49a78446c357c88d7410ad7dd902bcbcfff26cc5
Smilepenguin <ms.lyuba97@mail.ru>
2020-11-27 14:35:45 +0300

    Графики с 5 и 10 тестами

---
+++ tests_for_numerical.R
@@ -0,0 +1,119 @@
+library(ggplot2)
+library(dplyr)
+library(ggthemes)
+library(scales)
+library(ggpubr)
+library(MatchIt)
+
+source("numerical_tests.r")
+
+step = 50
+step2 = 15
+max_T = 500
+All_tests<- seq(100, max_T, by = step)
+
+for (tests in 1:5) {
+
+  All_num_R<-c()
+  
+   
+  for (num_T in seq(100, max_T, by = step)){
+    P_values = 0
+    num_R <- num_T
+    while(P_values != tests){
+    P_values = 0
+    num_R <- num_R + step2
+    Patients <- c()
+    
+  for (k in 1:tests) {
+    test_name <- numerical_tests[k, "name"]
+    mu_T <- numerical_tests[k, "average_T"]
+    mu_R <- numerical_tests[k, "average_R"]
+    random_values <-
+      c(
+        round(rnorm(
+          num_T, mean = mu_T, sd = numerical_tests[k, "sd"]
+        ), digits = numerical_tests[k, "digits"]),
+        round(rnorm(
+          num_R, mean = mu_R, sd = numerical_tests[k, "sd"]
+        ), digits = numerical_tests[k, "digits"])
+      )
+    Patients <- cbind(Patients, random_values)
+    colnames(Patients)[k] <- numerical_tests[k, "name"] 
+  }
+  
+ 
+  
+  Patients <- data.frame(Patients)
+
+  
+  Patients$ID <- c(1:(num_R + num_T))
+  
+  Patients$Group <-  as.logical(c(rep(1, num_T),
+                                  rep(0, num_R)), levels = c('R', 'T'))
+  
+  
+  
+  fmla <- as.formula(paste("Group ~ ", paste(colnames(Patients)[1:(ncol(Patients)-2)], collapse = "+")))
+  
+  mod_match <-
+    matchit(
+      fmla,
+      method = "nearest",
+      data = Patients
+    )
+  newdf <- match.data(mod_match)
+  library(tidyr)
+  New_Patients_for_plot <- newdf %>%
+    pivot_longer(cols = numerical_tests[c(1:tests),1],
+                 names_to = "names",
+                 values_to = "value")
+  
+  newdf$Group <-  factor(newdf$Group, labels = c('R', 'T'))
+ 
+  for (z in 1:tests){
+    if (
+      wilcox.test(newdf[,z] ~ Group, newdf, var.equal = TRUE)$p.value > 0.05 )
+    P_values <- P_values + 1  
+      }
+  
+    }
+    All_num_R <- rbind(All_num_R, num_R)
+    print(num_R)
+  }
+  All_tests <- cbind(All_tests, All_num_R)
+}
+
+All_tests <- data.frame(All_tests)
+colnames(All_tests)[1] <- "number_T"
+
+ggplot(All_tests, aes(x = number_T)) + 
+  geom_point(size = 1, aes(y = V2)) + 
+  geom_line( alpha= 0.5, aes(y = V2, color="num_tests = 1")) +
+  geom_point(size = 1, aes(y = V3)) + 
+  geom_line( alpha= 0.5, aes(y = V3, color="num_tests = 2")) +
+  geom_point(size = 1, aes(y = V4)) + 
+  geom_line( alpha= 0.5, aes(y = V4, color="num_tests = 3")) +
+  geom_point(size = 1, aes(y = V5)) + 
+  geom_line( alpha= 0.5, aes(y = V5, color="num_tests = 4")) +
+  geom_point(size = 1, aes(y = V6)) + 
+  geom_line( alpha= 0.5, aes(y = V6, color="num_tests = 5")) +
+  theme_bw() +
+  theme() +
+  ylab("„исло пациентов, принимающих плацебо") +
+  xlab("„исло пациентов, принимающих лекарства") +
+  ggtitle(" оличество пациентов при разном количестве тестов") +
+  scale_colour_manual("„исло тестов", values = c('red','blue', 'orange', 'green', 'yellow')) +
+  scale_x_continuous(breaks = seq(100, max_T, step)) +
+  scale_y_continuous(breaks = seq(100, 3000, step2)) +
+  theme(strip.text.x = element_text(size = 10, face = 'bold'),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 10))
+  
+  
+  
+  #save(All_tests, file = "All_tests_wilcox.RData")
