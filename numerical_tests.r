c0cbc9745ca6125e4e81628b6d301c95cb47a96f
Smilepenguin <ms.lyuba97@mail.ru>
2020-11-27 14:33:04 +0300

    Нормализовали разбросы

--- numerical_tests.r
+++ numerical_tests.r
@@ -1,7 +1,7 @@
 numerical_tests <- data.frame(
   name = c("Temperature", "Pulse", "Hemoglobin", "BMI", "Leu", "Sugar", "Age"),
-  average_T = c(37, 70, 130, 37.5, 6.702, 6.7, 40),
-  average_R = c(36.4, 75, 140, 35.5, 6.5, 6.3, 38),
+  average_T = c(36.75, 70,   130,   39.7, 6.702, 7, 41.7),
+  average_R = c(36.4,  74.2, 142.5, 34.2, 6.5,   6, 37),
   sd = c(0.5, 6, 18, 8, 1.45, 1.11, 6.8),
   digits = c(1,0, 0, 2, 1, 2, 0),
   stringsAsFactors = FALSE
